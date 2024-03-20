b45da835aa5da99d0137c8393913c379a9f4208c
Dima Usov <dmitry.usov@yahoo.com>
2024-03-20 11:40:20 +0300

    feat: add getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -214,8 +214,17 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const day = date.getDay();
+  const nextFriday = new Date(date);
+
+  const daysToNextFriday = 5 - day;
+  nextFriday.setDate(nextFriday.getDate() + daysToNextFriday);
+
+  while (nextFriday.getDate() !== 13) {
+    nextFriday.setDate(nextFriday.getDate() + 7);
+  }
+  return nextFriday;
 }
 
 /**
